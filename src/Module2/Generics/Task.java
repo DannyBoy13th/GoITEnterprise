85d5f054fd0cd16778ed63c0116ee13111c9fe06
Daniel Solo <danielsolo13th@gmail.com>
2016-06-07 23:48:35 +0300

    Scetches for the module 2

---
+++ src/Module2/Generics/Task.java
@@ -0,0 +1,15 @@
+package Module2.Generics;
+
+/**
+ * Created by Daniel Solo on 07.06.2016.
+ */
+public interface Task {
+
+    // Метода запускает таск на выполнение
+    void execute();
+
+    // Возвращает результат выполнения
+    Object getResult();
+
+
+}
---
+++ src/Module2/Generics/Validator.java
@@ -0,0 +1,11 @@
+package Module2.Generics;
+
+/**
+ * Created by Daniel Solo on 07.06.2016.
+ */
+public interface Validator {
+
+    // Валидирует переданое значение
+    boolean isValid(Object result);
+
+}
